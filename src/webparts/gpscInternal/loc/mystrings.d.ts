11074ba483bf99d340ea9cd700cdb3d1fa34df37
Boripan Phormsoonthorn <boripan.p@tora-asia.com>
2020-09-29 17:39:02 +0700

    Create new project GPSC internal

---
+++ src/webparts/gpscInternal/loc/mystrings.d.ts
@@ -0,0 +1,10 @@
+declare interface IGpscInternalWebPartStrings {
+  PropertyPaneDescription: string;
+  BasicGroupName: string;
+  DescriptionFieldLabel: string;
+}
+
+declare module 'GpscInternalWebPartStrings' {
+  const strings: IGpscInternalWebPartStrings;
+  export = strings;
+}
